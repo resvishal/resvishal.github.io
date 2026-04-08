1c653a35b303a3c9c2057b95b876f410eb96b901
resvishal <131158705+resvishal@users.noreply.github.com>
2026-04-08 11:39:56 +0000

    Deploying to gh-pages from @ resvishal/resvishal.github.io@e5ca0efd5705185ef11e15d0c334d977c71da2b3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -34,11 +34,11 @@ ninja.data = [{
             window.location.href = "/blog/2026/phd-applications-abroad/";
           
         },
-      },{id: "post-molecular-dynamics-for-beginners-watching-proteins-move-step-by-step",
+      },{id: "post-molecular-dynamics-workflow-tutorial-md2026-04-05-molecular-dynamics-workflow-tutorial",
         
-          title: "Molecular Dynamics for Beginners: Watching Proteins Move (Step-by-Step)",
+          title: "Molecular Dynamics Workflow Tutorial.md2026 04 05 Molecular Dynamics Workflow Tutorial",
         
-        description: "A simple, beginner-friendly introduction to running your first MD simulation",
+        description: "",
         section: "Posts",
         handler: () => {
           
